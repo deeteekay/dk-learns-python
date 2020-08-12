88287cd7094b787d95fdbb65e99d42cf854d09d6
deeteekay <69543593+deeteekay@users.noreply.github.com>
2020-08-12 15:29:56 -0400

    Update IsItRaining.py
    REMOVED excessive white space

--- IsItRaining.py
+++ IsItRaining.py
@@ -2,27 +2,15 @@
 # coding: utf-8
 
 # In[3]:
-
-
 def IsItRaining():
     IsRaining = input ("Is it raining today [y/n]?" )
     return IsRaining
 
-
 # In[5]:
-
-
 mondayRain = IsItRaining()
 
-
 # In[6]:
-
-
 print(mondayRain)
-
-
-# In[ ]:
-
 
 
 
