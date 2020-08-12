735e4c9ad90c363d66b1809747ea638c8d87782e
deeteekay <69543593+deeteekay@users.noreply.github.com>
2020-08-12 15:27:02 -0400

    Create IsItRaining.py

---
+++ IsItRaining.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def IsItRaining():
+    IsRaining = input ("Is it raining today [y/n]?" )
+    return IsRaining
+
+
+# In[5]:
+
+
+mondayRain = IsItRaining()
+
+
+# In[6]:
+
+
+print(mondayRain)
+
+
+# In[ ]:
+
+
+
+
